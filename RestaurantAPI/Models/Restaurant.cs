d3aab82ad6b61bc110cf43be9133b5a8b2f2d8c0
anujgupta2676 <anujrox960@gmail.com>
2022-06-30 18:28:15 +0530

    first

---
+++ RestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string RestaurantName { get; set; }
+        
+    }
+}
